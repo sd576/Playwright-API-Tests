069fed01c3b2346e85881d7dd7c9f36f556eb27b
sd576 <sd576@btinternet.com>
2025-02-04 14:27:57 +0000

    added complete and passing test getAllSettlement.spec.ts

--- src/tests/api/getAllSettlements.spec.ts
+++ src/tests/api/getAllSettlements.spec.ts
@@ -1,6 +1,22 @@
 import { test, expect } from "../../hooks/apiHooks";
 import { compareData } from "../../fixtures/compareData";
-import counterpartyData from "../../../reference-data/counterpartyData.json";
-import nostroData from "../../../reference-data/nostroData.json";
+import settlementData from "../../../reference-data/settlementData.json";
+import { resolve } from "path";
 
 test.describe.configure({ mode: "serial" });
+
+test("GET All FX Settlement Nostro accounts", async ({ request }) => {
+  console.log("Waiting for the database to settle...");
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
+  console.log("Fetching all the FX Nostro accounts");
+  const response = await request.get("http://localhost:3000/api/settlements");
+
+  console.log("GET Response Status: ", response.status());
+  console.log("GET Response Body: ", await response.text());
+
+  expect(response.status()).toBe(200);
+
+  const responseBody = await response.json();
+  await compareData(responseBody, settlementData);
+});
